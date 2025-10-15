bcfabb3fc7ad4c713e1767ffb5714eb1c89aa2b2
Polinka Prokopenko <prokopenkopolinka@outlook.com>
2025-10-15 22:39:46 +0300

    Zxing packages are added,  project updated to .NET 9

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,28 +1,33 @@
 ﻿using Microsoft.Extensions.Logging;
+using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
 
 namespace RegisterLogin;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				fonts.AddFont("AlanSans-ExtraBold.ttf", "AlanSansExtraBold");
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("AlanSans-ExtraBold.ttf", "AlanSansExtraBold");
                 fonts.AddFont("AlanSans-Regular.ttf", "AlanSansRegular");
                 fonts.AddFont("DMSans-Regular.ttf", "DMSansRegular");
                 fonts.AddFont("DMSans-ExtraBold.ttf", "DMSansExtraBold");
-            });
+            })
+            .UseBarcodeReader();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
--- Views/ScanItemPage.xaml.cs
+++ Views/ScanItemPage.xaml.cs
@@ -1,4 +1,5 @@
 using System.Net.NetworkInformation;
+using ZXing.Net.Maui;
 
 namespace RegisterLogin.Views;
 
@@ -7,5 +8,19 @@ public partial class ScanItemPage : ContentPage
 	public ScanItemPage()
     {
         InitializeComponent();
+        BarcodeReader.Options = new ZXing.Net.Maui.BarcodeReaderOptions
+        {
+            Formats = BarcodeFormat.QrCode,
+            AutoRotate = true,
+            Multiple = true
+        };
 	}
+
+    private void BarcodeReader_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+    {
+        var first = e.Results.FirstOrDefault();
+        if (first == null)
+            return;
+;
+    }
 }
